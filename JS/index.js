aa59db0b34ef7acf61eb33da7d3753f2906fdd4f
MrBarker47 <32359477+MrBarker47@users.noreply.github.com>
2023-03-30 13:46:47 -0400

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,19 @@
+let lightMode = localStorage.getItem('toggle')
+
+const toggle = document.getElementById('toggle');
+const body = document.body;
+
+
+
+
+
+
+toggle.addEventListener('input', (e) => {
+	const isChecked = e.target.checked;
+	
+	if(isChecked) {
+		body.classList.add('light-theme');
+	} else {
+		body.classList.remove('light-theme');
+	}
+});
