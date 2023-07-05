fcd2c92646a710da6bd2950c547d369d240f33db
MrBarker47 <32359477+MrBarker47@users.noreply.github.com>
2023-07-05 09:04:17 -0400

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,37 @@
-let lightMode = localStorage.getItem('toggle')
+// let lightMode = localStorage.getItem("lightMode");
 
-const toggle = document.getElementById('toggle');
-const body = document.body;
+// const toggle = document.getElementById('toggle');
+// const body = document.body;
 
+// const enableLightMode = () => {
+// 	//1. add the class lightmode to the body
+// 	document.body.classList.add("lightmode");
+// 	// 2. update lightmode in the localStorage
+// 	localStorage.setItem("lightMode", "enabled");
+// };
 
+// const disableLightMode = () => {
+// 	// 1. add the class lightmode to the body
+// 	document.body.classList.remove("lightmode");
+// 	// 2. update lightmode in the localStorage
+// 	localStorage.setItem("lightMode", null);
+// };
 
+// //if the user already vistied and enabled lightMode
+// if (lightMode === 'enabled ') {
+// 	enableLightMode();
+// }
 
-
-
-toggle.addEventListener('input', (e) => {
-	const isChecked = e.target.checked;
-	
-	if(isChecked) {
-		body.classList.add('light-theme');
-	} else {
-		body.classList.remove('light-theme');
-	}
-});
+// toggle.addEventListener('click', (e) => {
+// 	const isChecked = e.target.checked;
+// 	darkMode = localStorage.getItem("lightMode");
+// 	if(isChecked) {
+// 		body.classList.add('light-theme');
+// 		enableLightMode();
+// 		console.log(lightMode);
+// 	} else {
+// 		body.classList.remove('light-theme');
+// 		disableLightMode();
+// 		console.log(lightMode);
+// 	}
+// });
